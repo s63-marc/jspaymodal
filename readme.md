4eda973b6302ca6bc59eaefd0a7c473a15a14e21
Gian Carlo Val Flores Ebao <gianebao@reddotpay.com>
2018-12-06 10:17:46 +0800

    initial

---
+++ readme.md
@@ -0,0 +1,33 @@
+# RDP Modal
+
+Allows the Red Dot Payment (RDP) hosted payment and card capture page to be embedded to an HTML page.
+
+## Usage
+
+~~~
+<!doctype html>
+<html lang="en">
+  <head>
+    ...
+  </head>
+  <body>
+    <button type="button" id="pay">Pay</button> 
+
+    <script src="src/modal.js"></script>
+    <script type="text/javascript">
+        document.getElementById('pay').addEventListener('click', function (e) {
+            RDPModal.pay();
+        });
+    </script>
+  </body>
+</html>
+~~~
+
+## Open-source:
+
+1. Support only modern browsers
+2. Do not add any library dependencies
+3. Use only vanilla codes
+4. Do not do any post processing (obfuscation, minimizing, etc.)
+5. Have fun with the preloaders
+
---
+++ src/modal.js
@@ -0,0 +1,123 @@
+var RDPModal = (function() {
+    const csspath = 'modal.css3.css';
+    const rdpid = 'rdp-modal';
+    const hidden = 'hidden';
+    const displaynone = 'displaynone'; // cleaning purposes
+    
+    const transitionEvents = (function(){
+        var i, 
+        el = document.createElement('DIV');
+        transitions = {
+            'transition': ['transitionstart', 'transitionend'],
+            'OTransition': ['otransitionstart', 'otransitionend'], // oTransitionEnd in very old Opera
+            'MozTransition': ['transitionstart', 'transitionend'],
+            'WebkitTransition': ['webkitTransitionStart', 'webkitTransitionEnd']
+        };
+        
+        for (i in transitions) {
+            if (el.hasOwnProperty(i) || el.style.hasOwnProperty(i)) {
+                return {'start': transitions[i][0], 'end': transitions[i][1]};
+            }
+        }
+        
+        return console.error('transitionend event not supported. browser update required');
+    })();
+
+    (function() {
+        var link = document.createElement('link');
+        link.href = csspath;
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.media = 'screen,print';
+        document.getElementsByTagName('head')[0].appendChild(link);
+    })();
+    
+    var m = {
+            modal: null,
+            frame: null,
+            spinner: null,
+            close: null,
+            closeTriggers:[],
+            fn: {}
+        };
+
+    m.fn.init = function() {
+        if (null != m.modal) {
+            return
+        }
+
+        m.modal = document.createElement('DIV');
+        m.modal.setAttribute('id', rdpid);
+        m.modal.classList.add(hidden);
+        m.modal.innerHTML = '<div class="content"><span class="close hidden">×</span><div class="frame"><div class="loader"></div><iframe class="hidden"></iframe></div></div>';
+        m.close = m.modal.querySelector('.close');
+        m.spinner = m.modal.querySelector('.loader');
+        m.frame = m.modal.getElementsByTagName('iframe')[0];
+        m.closeTriggers = [m.modal, m.close];
+        document.getElementsByTagName('body')[0].appendChild(m.modal);
+
+        m.modal.addEventListener('click', function(e) {
+            m.closeTriggers.forEach(el => {
+                if (e.target === el) {
+                    m.fn.close();
+                    return false;
+                }
+            });
+        });
+
+        m.modal.addEventListener(transitionEvents.end, function (e) {
+            var cl = m.modal.classList;
+            if (cl.contains(hidden) && !cl.contains(displaynone)) {
+                cl.add(displaynone);
+            }
+        });
+
+        m.frame.addEventListener('load', function (e) {
+            if (!m.spinner.classList.contains(hidden)){
+                m.frame.classList.remove(hidden);
+                m.close.classList.remove(hidden);
+                m.spinner.classList.add(hidden);
+            }
+        });
+    };
+
+    m.fn.close = function() {
+        if (!m.modal.classList.contains(hidden)) {
+            m.modal.classList.add(hidden);
+        }
+    };
+
+    var opening = false;
+    m.fn.open = function() {
+        m.fn.init();
+        var cl = m.modal.classList;
+
+        if (!opening && cl.contains(hidden)) {
+            opening = true;
+            cl.remove(displaynone);
+            m.frame.classList.add(hidden);
+            m.close.classList.add(hidden);
+            m.spinner.classList.remove(hidden);
+            
+            setTimeout(function () {
+                // delay hack for smoother transition while removing display:none
+                cl.remove(hidden);
+                opening = false;
+            }, 1);
+            
+        }
+    };
+
+    m.fn.toggle = function() {
+        m.fn.init();
+        m.modal.classList.toggle(hidden);
+    };
+    
+    return {
+        toggle: m.fn.toggle,
+        pay: function () {
+            m.fn.open();
+            m.frame.src = 'https://google.com';
+        }
+    };
+})();
