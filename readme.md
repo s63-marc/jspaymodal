1cccd748d4a4ff68e76109519e3e33744187bfe0
Gian Carlo Val Flores Ebao <gianebao@reddotpay.com>
2018-12-20 16:47:59 +0800

    cleanup the file

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# RDP Javascript Pay Modal
+# RDP Javascript Pay [Modal]
 
 Allows the Red Dot Payment (RDP) hosted payment and card capture page to be embedded to an HTML page.
 
@@ -8,32 +8,39 @@ Allows the Red Dot Payment (RDP) hosted payment and card capture page to be embe
 
 ## Example
 
-- [example.html](https://reddotpay.github.io/jspaymodal/example.html)
+- [example.html](https://reddotpay.github.io/jspay/example.html)
 
 ## Usage
 
+### modal
 ~~~
 <!doctype html>
 <html lang="en">
   <head>
     ...
+    <!-- Use an optional -->
+    <link rel="stylesheet" type="text/css" media="screen,print" href="https://reddotpay.github.io/jspay/modal.loader2.css3.css">
   </head>
   <body>
     <button type="button" id="pay">Pay</button> 
 
-    <script src="src/modal.js"></script>
+    <script src="https://reddotpay.github.io/jspay/src/jspay.js"></script>
     <script type="text/javascript">
+        // You can overwrite the base CSS file:
+        // RDP.modal.init('https://myowndomain.com/assets/modal.css3.css');
+        RDP.modal.init();
+
+        // To switch to PRODUCTION:
+        // RDP.authDomain = 'https://connect.api.reddotpay.com';
+        // RDP.domain = 'https://connect.reddotpay.com';
         document.getElementById('pay').addEventListener('click', function (e) {
-            RDPModal.pay({
-                mid: "00000000-0000-0000-0000-000000000000",
-                payload: {
-                    amount: 37.76,
-                    currency: "SGD",
-                    email: "someone@email.com",
-                    promotion: "promocode",
-                    orderId: "OID" + (new Date()).getTime() // Must be unique
-                }
-            });
+            RDP.modal.pay(
+                "OID" + (new Date()).getTime(), // Order ID
+                "00000000-0000-0000-0000-000000000000", // Merchant ID
+                37.76, // Amount
+                "SGD", // SGD
+                { promo: "HAPPYNEWYEAR01" } // Other details
+            );
         });
     </script>
   </body>
---
+++ src/jspay.js
@@ -0,0 +1,192 @@
+"use strict";
+
+const RDP = (() => {
+    const Modal = class {
+        // modal;
+        // closeButton;
+        // spinner;
+        // frame;
+        
+        // cssHidden;
+        // cssDisplayNone;
+        // opening;
+
+        constructor(id, css) {
+            this.cssHidden = 'hidden';
+            this.cssDisplayNone = 'displaynone';
+            this.opening = false;
+            this.id = id;
+
+            this.installCSS(css);
+        }
+
+        init() {
+            const modal = !this.modal ? this.createElement(this.id): document.getElementById(this.id);
+            this.modal = modal;
+            
+
+            this.closeButton = modal.querySelector('.close');
+            this.spinner = modal.querySelector('.loader');
+            this.frame = modal.getElementsByTagName('iframe')[0];
+            this.attachBasicBehaviours();
+        }
+
+        installCSS(css) {
+            const link = document.createElement('LINK');
+                link.setAttribute('href', css);
+                link.setAttribute('type', 'text/css');
+                link.setAttribute('rel', 'stylesheet');
+                link.setAttribute('media', 'screen,print');
+            
+            document.getElementsByTagName('head')[0].appendChild(link);
+        }
+
+        createElement(id) {
+            const modal = document.createElement('DIV');
+            modal.setAttribute('id', id);
+            modal.classList.add(this.cssHidden);
+            modal.innerHTML = '<div class="content"><span class="close hidden">×</span><div class="frame"><div class="loader"></div><iframe class="hidden"></iframe></div></div>';
+            
+            document.getElementsByTagName('body')[0].appendChild(modal);
+
+            return modal;
+        }
+        
+        attachBasicBehaviours() {
+            const modal = this.modal;
+            const frame = this.frame;
+            const spinner = this.spinner;
+            const closeButton = this.closeButton;
+            const close = this.close.bind(this);
+            const hidden = this.cssHidden;
+            const displayNone = this.cssDisplayNone;
+
+            modal.addEventListener('click', e => {
+                [modal, closeButton].forEach(el => {
+                    if (e.target === el) {
+                        close();
+                        return false;
+                    }
+                });
+            });
+        
+            modal.addEventListener(this.getTransitionEvents().end, e => {
+                const cl = modal.classList;
+                if (cl.contains(hidden) && !cl.contains(displayNone)) cl.add(displayNone);
+            });
+        
+            frame.addEventListener('load', e => {
+                if (!e.target.src || spinner.classList.contains(hidden)) return;
+                frame.classList.remove(hidden);
+                closeButton.classList.remove(hidden);
+                spinner.classList.add(hidden);
+            });
+        }
+
+        close() {
+            const hidden = this.cssHidden;
+            if (!this.modal.classList.contains(hidden)) this.modal.classList.add(hidden);
+        }
+
+        open() {
+            if (!this.modal) this.init();
+            const cl = this.modal.classList;
+            const hidden = this.cssHidden;
+            const displayNone = this.cssDisplayNone;
+
+            if (!this.opening && cl.contains(hidden)) {
+                this.opening = true;
+                cl.remove(displayNone);
+                this.spinner.classList.remove(hidden);
+                this.frame.classList.add(hidden);
+                this.closeButton.classList.add(hidden);
+
+                setTimeout(() => {
+                    // delay hack for smoother transition while removing display:none
+                    cl.remove(hidden);
+                    this.opening = false;
+                }, 1);
+            }
+        }
+
+        toggle() {
+            this.modal.classList.toggle(this.cssHidden);
+        }
+
+        getTransitionEvents() {
+            const el = document.createElement('DIV');
+            const transitions = {
+                    'transition': ['transitionstart', 'transitionend'],
+                    'OTransition': ['otransitionstart', 'otransitionend'], // oTransitionEnd in very old Opera
+                    'MozTransition': ['transitionstart', 'transitionend'],
+                    'WebkitTransition': ['webkitTransitionStart', 'webkitTransitionEnd']
+                };
+            
+            let i;
+            for (i in transitions) {
+                if (el.hasOwnProperty(i) || el.style.hasOwnProperty(i) || i in el.style) {
+                    return {'start': transitions[i][0], 'end': transitions[i][1]};
+                }
+            }
+            
+            return console.error('transitionend event not supported. browser update required');
+        }
+    }
+
+    const Pay = class {
+        // merchant;
+        // authDomain;
+
+        constructor(merchant, authDomain) {
+            this.authDomain = authDomain;
+            this.merchant = merchant;
+        }
+
+        do(id, amount, currency, options) {
+            if ('object' != options) options = {};
+            options['orderId'] = id;
+            options['amount'] = amount;
+            options['currency'] = currency;
+
+            return fetch(this.authDomain + '/api/v1/payments/token/' + this.merchant, {
+                method: 'POST',
+                credentials: 'same-origin',
+                mode: 'cors',
+                headers: { 'Content-Type': 'application/json; charset=utf-8' },
+                body: JSON.stringify(options)
+            })
+            .then(res => {
+                return res.json();
+            });
+        }
+    }
+
+    let modal;
+
+    const lib = {
+        authDomain: 'https://api-pay-redirect.herokuapp.com',
+        domain: 'https://connect.reddotpay.sg',
+
+        modal: {
+            init: (css) => {
+                modal = new Modal('rdp-modal', css ? css: 'https://reddotpay.github.io/jspay/modal.css3.css');
+            },
+
+            pay: (id, merchant, amount, currency, options) => {
+                modal.open();
+                return lib.callback(id, merchant, amount, currency, options, src => {
+                    modal.frame.setAttribute('src', src);
+                });
+            },
+        },
+
+        callback(id, merchant, amount, currency, options, okFn) {
+            const pay = new Pay(merchant, lib.authDomain);
+            return pay
+                .do(id, amount, currency, options)
+                .then(auth => { okFn(lib.domain + '/m/'+merchant+'#/pay/' + auth.token); });
+        }
+    };
+
+    return lib;
+})();
