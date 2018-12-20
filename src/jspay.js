98ebc599e05787e2ce9a940a5e0be8b88398247c
Gian Carlo Val Flores Ebao <gianebao@reddotpay.com>
2018-12-20 18:08:53 +0800

    update error handling

--- src/jspay.js
+++ src/jspay.js
@@ -156,7 +156,9 @@ const RDP = (() => {
                 body: JSON.stringify(options)
             })
             .then(res => {
-                console.log(res);
+                if (!res.ok) {
+                    throw Error(res.status + ':' +res.statusText);
+                }
                 return res.json();
             });
         }
