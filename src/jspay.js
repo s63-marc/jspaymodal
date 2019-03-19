6640f6aa4a00d8ac444040c7094d4f6446d32bc4
Gian Carlo Val Flores Ebao <gianebao@reddotpay.com>
2019-03-19 13:06:10 +0800

    fixed dimensions of svg

--- src/jspay.js
+++ src/jspay.js
@@ -46,7 +46,7 @@ const RDP = (() => {
             modal.setAttribute('id', id);
             modal.classList.add(this.cssHidden);
             modal.innerHTML = '<div class="content"><span class="close hidden">' +
-                '<svg viewPort="0 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg">' +
+                '<svg viewPort="0 0 12 12" height="12" width="12" version="1.1" xmlns="http://www.w3.org/2000/svg">' +
                 '<line x1="1" y1="11" x2="11" y2="1" stroke="black" stroke-width="2"/>' +
                 '<line x1="1" y1="1" x2="11" y2="11" stroke="black" stroke-width="2"/>' +
                 '</svg></span><div class="frame"><div class="loader"></div>' +
